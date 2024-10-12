dcd8ea83acedad137e8431b142157e6d4433660f
ABDULHAMEED IDRIS ADEDAMOLA <36794582+idris0001@users.noreply.github.com>
2024-10-12 11:40:03 +0900

    Add files via upload
    Attached are the two files needed to implement the 'rainbow' application, in addition to the output.

---
+++ DrawRainbowTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.drawrainbow;
+
+/**
+ *
+ * @author Monet
+ */
+// Fig. 7.26: DrawRainbowTest.java
+ // Test application to display a rainbow.
+import javax.swing.JFrame;
+
+ public class DrawRainbowTest
+ {
+ public static void main( String[] args )
+ {
+ DrawRainbow panel = new DrawRainbow();
+ JFrame application = new JFrame();
+
+ application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+ application.add( panel );
+  application.setSize( 400, 250 );
+  application.setVisible( true );
+  } // end main
+  } // end class DrawRainbowTest
